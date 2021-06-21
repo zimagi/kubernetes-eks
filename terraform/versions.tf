89e4cfa46a9b83a986625d7c36e5352a8000374a
Erik Jagyugya <jagyugyaerik@gmail.com>
2021-06-21 20:27:52 +0200

    Replace original aws module with cloudposse's module
    Cloudposse makes terraform modules professionaly. Most of the modules are
    well tested and secure.
    
    Signed-off-by: Erik Jagyugya <jagyugyaerik@gmail.com>

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,19 +1,26 @@
 terraform {
-  required_version = ">= 0.14"
-}
+  required_version = ">= 0.13.0"
 
-provider "random" {
-  version = "~> 2.1"
-}
-
-provider "local" {
-  version = "~> 1.2"
-}
-
-provider "null" {
-  version = "~> 2.1"
-}
-
-provider "template" {
-  version = "~> 2.1"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 2.0"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = ">= 2.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 2.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 1.3"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0"
+    }
+  }
 }
